2b58e72139076f34782723c3432fb9befe5ed94a
CrispyCroissant <gentlemug00@gmail.com>
2022-06-22 21:43:06 +0200

    Remove unnecessary endpoint

--- backend/src/routers/index.spec.ts
+++ backend/src/routers/index.spec.ts
@@ -1,28 +1,14 @@
 import request from "supertest";
 import app from "../app";
 
+jest.mock("axios");
+
 describe("GET /api/image", () => {
   const URL = "/api/image";
 
   it("exists", async () => {
     const res = await request(app).get(URL);
 
-    expect(res.statusCode).toBe(200);
-  });
-});
-
-describe("POST /api/image", () => {
-  const URL = "/api/image";
-
-  it("exists", async () => {
-    const res = await request(app).post(URL);
-
-    expect(res.statusCode).toBe(400);
-  });
-
-  it("returns 400 if no image hash is provided", async () => {
-    const res = await request(app).post(URL).send();
-
-    expect(res.statusCode).toBe(400);
+    expect(res.statusCode).toBe(500);
   });
 });
