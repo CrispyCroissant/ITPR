4e5a486a38f6169dbd5f8e5e9d5ed18276ced060
CrispyCroissant <gentlemug00@gmail.com>
2022-07-19 16:18:05 +0200

    Use footer

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import HomePage from "./pages/HomePage.vue";
+import TheFooter from "./components/TheFooter.vue";
 </script>
 
 <template>
   <main>
     <HomePage></HomePage>
+    <TheFooter></TheFooter>
   </main>
 </template>
 
