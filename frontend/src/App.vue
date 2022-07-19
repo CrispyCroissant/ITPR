7fdab8116f0e10ebb3266258090d16d6ae4f9c12
CrispyCroissant <gentlemug00@gmail.com>
2022-07-19 17:50:24 +0200

    Refactor main file

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
-import HomePage from "./pages/HomePage.vue";
 import TheFooter from "./components/TheFooter.vue";
 </script>
 
 <template>
   <main>
-    <HomePage></HomePage>
+    <RouterView></RouterView>
     <TheFooter></TheFooter>
   </main>
 </template>
