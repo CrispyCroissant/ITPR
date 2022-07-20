5d71cb93bb98a008ff2ec8c3f59629ec90fe8bd4
CrispyCroissant <gentlemug00@gmail.com>
2022-07-20 13:42:03 +0200

    Create game page skeleton

---
+++ frontend/src/pages/GamePage.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import AppButton from "../components/AppButton.vue";
+</script>
+
+<template>
+  <div class="bg-image"></div>
+  <div class="flex column">
+    <h1 class="title__big">Is this person real?</h1>
+    <div class="flex">
+      <img src="https://picsum.photos/600" alt="Photo of a person" />
+      <div class="flex">
+        <AppButton class="red">Not real</AppButton>
+        <AppButton class="green">Real</AppButton>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
