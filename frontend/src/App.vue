b7442c8d6c2789d784ddcca6e1196dfebb40a174
CrispyCroissant <gentlemug00@gmail.com>
2022-07-20 19:14:56 +0200

    Refactor background image into its own component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import TheFooter from "./components/TheFooter.vue";
+import TheBackgroundImage from "./components/TheBackgroundImage.vue";
 </script>
 
 <template>
   <main>
-    <div class="bg-image"></div>
+    <TheBackgroundImage></TheBackgroundImage>
     <RouterView></RouterView>
     <TheFooter></TheFooter>
   </main>
